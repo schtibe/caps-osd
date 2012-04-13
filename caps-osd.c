2436d7c8777b52cc094bf6f0aabfa2718d7c4cb2
Stefan Heinemann <stefan.heinemann@codedump.ch>
2012-04-13 18:16:11 +0200

    Changed the whole thing to use the SIGUSR1 for toggling the OSD

--- caps-osd.c
+++ caps-osd.c
@@ -4,57 +4,51 @@
 #include <unistd.h>
 #include <xosd.h>
 
-int main (int argc, char *argv[]) {
+xosd *osd;
 
-	FILE *fp;
-	fp = fopen("/tmp/capser", "r");
-	if (fp != NULL) {
-		char pidstr[10];
-		if (fgets(pidstr, 9, fp) == NULL) {
-			exit(1);
-		}
-		printf("Read PID string %s\n", pidstr);
-		int pid = atoi(pidstr);
-		kill(pid, SIGINT);
-		fclose(fp);
-		remove("/tmp/capser");
+int osd_shown = 0;
 
-		exit(0);
-	}
-	
-	pid_t pid = fork();
-	if (pid == 0) {
-		xosd *osd;
-		
-		osd = xosd_create (2);
+void signaler(int s) {
+	printf("Caught signal %d\n", s);
 
-		if (osd == NULL) {
-			perror ("Could not create \"osd\"");
-			exit (1);
-		}
-
-		xosd_display(osd, 0, XOSD_string, "Capslock is ON");
-
-		xosd_set_font(osd, "-*-arial black-*-*-*-*-*-350-*-*-*-*-*-*");
-
-		xosd_set_pos(osd, XOSD_bottom);
-		xosd_set_align(osd, XOSD_center);
-
-		while (1) {
-			sleep(4);
-		}
-
-		xosd_destroy(osd);
-	
+	if (osd_shown) {
+		xosd_hide(osd);
+		osd_shown = 0;
 	} else {
-		fp = fopen("/tmp/capser", "w");
-		char pidstr[10];
-		sprintf(pidstr, "%d", pid);
-		fputs((char *)&pidstr, fp);
-		fclose(fp);
+		xosd_show(osd);
+		osd_shown = 1;
+	}
+}
 
-		exit(0);
+void registerSignal() {
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = signaler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGUSR1, &sigIntHandler, NULL);
+}
+
+int main (int argc, char *argv[]) {
+	registerSignal();
+
+	osd = xosd_create (2);
+
+	if (osd == NULL) {
+		perror ("Could not create \"osd\"");
+		exit (1);
 	}
 
-	exit(0);
+	xosd_display(osd, 0, XOSD_string, "Capslock is ON");
+
+	xosd_set_font(osd, "-*-arial black-*-*-*-*-*-350-*-*-*-*-*-*");
+
+	xosd_set_pos(osd, XOSD_bottom);
+	xosd_set_align(osd, XOSD_center);
+	xosd_hide(osd);
+
+	while (1) {
+		sleep(1);
+	}
+
+	return 0;
 }
